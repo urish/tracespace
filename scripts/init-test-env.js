fddf7feb585798b97b13c1bcddab547bc66bc5f1
Mike Cousins <mike@tracespace.io>
2019-10-06 19:37:30 -0400

    wip: ensure tests can run without build

--- scripts/init-test-env.js
+++ scripts/init-test-env.js
@@ -6,8 +6,29 @@ const EnzymeAdapter = require('enzyme-adapter-react-16')
 
 require('@babel/register')({
   configFile: path.join(__dirname, '../babel.config.js'),
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
-  extensions: ['.ts', '.d.ts', '.tsx'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          '@tracespace/parser': path.join(
+            __dirname,
+            '../packages/parser/src/index.ts'
+          ),
+          '@tracespace/plotter': path.join(
+            __dirname,
+            '../packages/plotter/src/index.ts'
+          ),
+          '@tracespace/renderer': path.join(
+            __dirname,
+            '../packages/renderer/src/index.ts'
+          ),
+        },
+      },
+    ],
+  ],
+  extensions: ['.ts', '.tsx'],
   sourceMaps: 'inline',
 })
 
